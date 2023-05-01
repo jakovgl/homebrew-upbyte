691de8a0a3dd5566bdeca4a0fe1842bdea3e1661
Jakov Glavas <jakov@init.hr>
2023-05-01 20:57:34 +0200

    update url and sha

--- Formula/upbyte.rb
+++ Formula/upbyte.rb
@@ -4,8 +4,8 @@
 class Upbyte < Formula
     desc "UpByte - Simple console monitoring tool for web apis"
     homepage "https://github.com/jglavas/upbyte"
-    url "https://github.com/jglavas/upbyte/releases/download/v0.1/upbyte.Console"
-    sha256 "a28921ba1314e3c9a8fd7acc584cb56bfdaa47fcc6e46b1eb2063e121a95b6c3"
+    url "https://github.com/jglavas/upbyte/archive/refs/tags/v0.1.tar.gz"
+    sha256 "a43cb60b241a52d1460b92a425cafd07a3d10ce2"
     version "v0.1"
 
     def install
