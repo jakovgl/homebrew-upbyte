053a5378a5d0ed331301f481fd56d57dfa4a30e7
Jakov Glavas <jakov@init.hr>
2023-05-01 12:45:18 +0200

    First release

---
+++ Formula/upbyte.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Upbyte < Formula
+    desc "UpByte - Simple console monitoring tool for web apis"
+    homepage "https://github.com/jglavas/upbyte"
+    url "https://github.com/jglavas/upbyte/archive/refs/tags/v0.1.tar.gz"
+    sha256 "12f2396ff2dda1c64ae26c0357f23820d3d8a206d9ca4ef02e78831bd8d97e31"
+    version "v0.1"
+
+    def install
+        bin.install "upbyte"
+    end
+end
