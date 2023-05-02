6a75e87b99c13db4371a46245bb438c1ccdd9e0e
Jakov Glavas <jakov@init.hr>
2023-05-02 22:47:16 +0200

    fix

--- Formula/upbyte.rb
+++ Formula/upbyte.rb
@@ -8,11 +8,11 @@ class Upbyte < Formula
     sha256 "a28921ba1314e3c9a8fd7acc584cb56bfdaa47fcc6e46b1eb2063e121a95b6c3"
     version "v0.1"
 
-    depends_on "dotnet-sdk" => :build
+    depends_on "dotnet" => :build
 
     def install
         system "dotnet", "publish", "-c", "Release", "-r", "osx-x64"
         libexec.install Dir["bin/Release/net6.0/osx-x64/*"]
-        bin.write_exec_script "#{libexec}/YourApp"
+        bin.write_exec_script "#{libexec}/upbyte"
     end
 end
