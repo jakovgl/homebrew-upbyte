6aaa8de93798f922ff8309ed9d91935b892ea8bd
Jakov Glavas <jakov@init.hr>
2023-05-03 22:06:33 +0200

    update install

--- Formula/upbyte.rb
+++ Formula/upbyte.rb
@@ -10,7 +10,6 @@ class Upbyte < Formula
 
     def install
         system "dotnet", "publish", "-c", "Release", "-r", "osx-x64"
-        libexec.install Dir["bin/Release/net6.0/osx-x64/*"]
-        bin.write_exec_script "#{libexec}/upbyte"
+        bin.install Dir["bin/Release/net6.0/osx-x64/*"]
     end
 end
