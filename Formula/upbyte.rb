3a9bdb302bf3566d8d73deb30d5d34e996c9dfc6
Jakov Glavas <jakov@init.hr>
2023-05-03 22:36:05 +0200

    fix

--- Formula/upbyte.rb
+++ Formula/upbyte.rb
@@ -1,15 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Upbyte < Formula
-    desc "UpByte - Simple console monitoring tool for web apis"
-    homepage "https://github.com/jglavas/upbyte"
-    url "https://github.com/jglavas/upbyte/archive/refs/tags/v0.1.tar.gz"
-    sha256 "a28921ba1314e3c9a8fd7acc584cb56bfdaa47fcc6e46b1eb2063e121a95b6c3"
-    version "v0.1"
+  desc "UpByte - Simple console monitoring tool for web apis"
+  homepage "https://github.com/jglavas/upbyte"
+  url "https://github.com/jglavas/upbyte/archive/refs/tags/v0.1.tar.gz"
+  sha256 "a28921ba1314e3c9a8fd7acc584cb56bfdaa47fcc6e46b1eb2063e121a95b6c3"
+  license "MIT"
+  version "0.1"
 
-    def install
-        system "dotnet", "publish", "-c", "Release", "-r", "osx-x64"
-        bin.install "UpByte.Console/bin/Release/net6.0/osx-x64/publish/UpByte.Console" => "upbyte"
-    end
+  depends_on "dotnet"
+
+  def install
+    system "dotnet", "publish", "-c", "Release", "-r", "osx-x64", "--self-contained", "true"
+    bin.install "UpByte.Console/bin/Release/net6.0/osx-x64/publish/UpByte.Console" => "upbyte"
+  end
+
+  test do
+    system "false"
+  end
 end
